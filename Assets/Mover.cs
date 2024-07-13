52faa6af04576a724fb53805f541fd93c91c952d
Louise Collins <louisecollins1971@icloud.com>
2024-07-13 09:32:45 +0100

    Start & Update

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Schema;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    
+    float xValue = 0;
+    float yValue = 0.01f;
+    float zValue = 0;
     void Start()
     {
         
@@ -12,6 +15,6 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(0.01f,0,0);
+        transform.Translate(xValue,yValue,zValue);
     }
 }
