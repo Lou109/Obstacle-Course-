989871ea8f738a2157ee1673ece253ed3e74e603
Louise Collins <louisecollins1971@icloud.com>
2024-07-14 01:14:08 +0100

    C# Formatting & Input.GetAxis()

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0;
-    [SerializeField] float yValue = 0.02f;
-    [SerializeField] float zValue = 0;
+    
     void Start()
     {
         
     }
 
-    
     void Update()
     {
-        transform.Translate(xValue,yValue,zValue);
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue,0,zValue);
+       
     }
 }
