1fcb09a25a44cb40682c9b7c0053b29806648b56
Louise Collins <louisecollins1971@icloud.com>
2024-07-17 15:49:46 +0100

    Incrementing a score

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -20,7 +20,7 @@ public class Mover : MonoBehaviour
     {
         Debug.Log("Welcome to the game");
         Debug.Log("Move your player with WASD or arrow keys");
-        Debug.Log("Don't hit the wall!");
+        Debug.Log("Don't hit the walls!");
     }
 
     void MovePlayer()
--- Assets/Scorer.cs
+++ Assets/Scorer.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectHit : MonoBehaviour
+public class Scorer : MonoBehaviour
 {
+   int hits = 0;
+
    private void OnCollisionEnter(Collision other) 
    {
-       GetComponent<MeshRenderer>().material.color = Color.white;
+       hits++;
+       Debug.Log ("You've bumped into a thing this many times: " + hits);
    }
 }
