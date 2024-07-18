283254e17cf267ea5bba44c63dabb28bbe0fc6e2
Louise Collins <louisecollins1971@icloud.com>
2024-07-18 02:50:01 +0100

    Using Time.time

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    int seconds = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {      
+            Debug.Log(Time.time);   
+    }
+}
